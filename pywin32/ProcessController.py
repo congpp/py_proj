939d3723943976679387fc0a98e483dc4e370d0d
conn-public <chen2cong@sina.com>
2022-08-30 14:31:20 +0800

    add pywin32

---
+++ pywin32/ProcessController.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: UTF-8 -*-
+import NtProcess
+import time
+
+if __name__ == '__main__':
+    #obj = ProcessUtil.ProcessSuspend('notepad.exe')
+    #time.sleep(5)
+    #ProcessUtil.ProcessTerminte('explorer.exe')
+    pm = NtProcess.ProcessMonitor()
+    proc = pm.getProcessEntries()
+    for p in proc:
+        print "%08x\t%s" % ( int(p.th32ProcessID), str(p.szExeFile) )
+
+    thrd = pm.getProcessThread('notepad.exe')
+    for t in thrd:
+        print '%08x\t%08x' % (int(t.th32ThreadID), int(t.th32OwnerProcessID))
